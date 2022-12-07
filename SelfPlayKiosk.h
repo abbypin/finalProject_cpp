20c0512c9be4d8762a7de56af6b3df6ae8dc1b49
abbypin <abby@techcleanup.com>
2022-12-07 13:15:20 -0800

    First commit

---
+++ SelfPlayKiosk.h
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+#include <iomanip>
+using namespace std;
+
+class SelfPlayKiosk {
+    private:
+    // Step 0
+        int customersServed;
+        double totalSales;
+        double currentAmountDue;
+        const int SALES_TAX = 0.7; // 7%
+    public:
+        // void scanItem();
+        // void cancelTransaction();
+        // void ckeckout();
+        // double makePayment();
+};
