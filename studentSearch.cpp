6cf2373ff3044a4d589e09809c9574accf5a39a1
abbypin <abby@techcleanup.com>
2022-12-15 17:01:37 -0800

    First Commit

---
+++ studentSearch.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+int main() {
+    string fileName = "";
+
+
+    return 0;
+}//end main()
+
+string FindID(string name, ifstream inFile) {
+
+
+}//end FindID
